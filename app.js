6be1a3cb762f1f163b3a905c972299dd120a7cf3
FelicityRC <felicitycarleton@hotmail.com>
2022-10-12 08:45:40 +0100

    get reviews by id with error handling and tests passing

--- app.js
+++ app.js
@@ -1,29 +1,33 @@
 const express = require("express");
 
-const { getCategories } = require("./controllers/controllers");
+const { getCategories } = require("./controllers/categories.controllers");
+const { getReviewById } = require("./controllers/reviews.controllers");
 
 const app = express();
 
 app.use(express.json());
 
 app.get("/api/categories", getCategories);
+app.get("/api/reviews/:review_id", getReviewById);
 
-app.all("/*", (request, response) => {
-  response.status(404).send({ msg: "not found" });
-  });
 
 app.use((err, request, response, next) => {
-  if (err.status === '22P02') {
-    response.status(400).send({ msg: "Invalid Id" });
+  if (err.code === "22P02") {
+    response.status(400).send({ msg: "invalid id" });
+  } else {
+    next(err);
   }
-  next(err);
+});
+app.all("/*", (request, response) => {
+  response.status(404).send({ msg: "not found" });
 });
 
 app.use((err, request, response, next) => {
-  if (err.status && err.msg) {
+  if (err.status) {
     response.status(err.status).send({ msg: err.msg });
+  } else {
+    next(err);
   }
-  next(err);
 });
 
 app.use((err, request, response, next) => {
