2e42a52dc2931bede29d2000d9b3d437e0770fb3
FelicityRC <felicitycarleton@hotmail.com>
2022-10-13 13:44:00 +0100

    add comment count query to retrieve corresponding review_id's comments amount and add test for this

--- models/reviews.models.js
+++ models/reviews.models.js
@@ -1,14 +1,17 @@
 const db = require("../db/connection");
 
 function fetchReviewById(review_id) {
-  return db
-    .query(`SELECT * FROM reviews WHERE review_id = $1;`, [review_id])
-    .then(({ rows }) => {
-      if (rows.length === 0) {
-        return Promise.reject({ status: 404, msg: "not found" });
-      }
+  let queryStr = `SELECT reviews.*, COUNT(comments.review_id) ::INT AS comment_count FROM reviews
+  LEFT JOIN comments ON reviews.review_id = comments.review_id
+  WHERE reviews.review_id = $1 GROUP BY reviews.review_id;`;
+  return db.query(queryStr, [review_id]).then(({ rows }) => {
+    if (rows.length === 0) {
+      return Promise.reject({ status: 404, msg: "not found" });
+    } else {
       return rows[0];
-    });
+    }
+  });
+
 }
 
 function getNewVotes(review_id, inc_votes) {
@@ -21,11 +24,12 @@ function getNewVotes(review_id, inc_votes) {
         [inc_votes, review_id]
       )
       .then(({ rows }) => {
-        if(rows.length === 0) {
+        if (rows.length === 0) {
           return Promise.reject({ status: 404, msg: "not found" });
         }
         return rows[0];
       });
   }
 }
+
 module.exports = { fetchReviewById, getNewVotes };
